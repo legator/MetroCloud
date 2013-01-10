7fc250a704a2dc87ad3139b265ad5ace2291fdf9
legAToR <oleg.legator@gmail.com>
2013-01-10 18:45:13 +0200

    Box.net API

---
+++ Cloud API/Boxnet/v0.0.1/source/AddCreatedByComment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Boxnet
+{
+    public class AddCreatedByComment
+    {
+        public string user_id { get; set; }
+        public string name { get; set; }
+        public string small_thumb_api { get; set; }
+    }
+}
